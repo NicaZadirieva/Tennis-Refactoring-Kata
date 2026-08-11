7992fab461489bf66d961bda4432ac8ef12c5615
NicaZadirieva <no.demchenko@yandex.ru>
2026-08-11 21:59:46 +0300

    refactor(tennis2): выделила обычный вариант игры

--- python/tennis2.py
+++ python/tennis2.py
@@ -2,9 +2,23 @@ from player import Player
 
 
 class TennisGame2:
+    POINTS_TO_SCORE = {0: "Love", 1: "Fifteen", 2: "Thirty", 3: "Forty"}
+
     def __init__(self, player1: Player, player2: Player):
         self.player1 = player1
         self.player2 = player2
+
+    def _is_regular_score(self) -> bool:
+        """Проверяет, находится ли игра в состоянии обычного счета.
+
+        Обычный счет - это когда оба игрока набрали меньше 4 очков
+        и их очки не равны (равный счет обрабатывается отдельно как Deuce).
+        """
+        return (
+            self.player1.points < 4
+            and self.player2.points < 4
+            and self.player1.points != self.player2.points
+        )
 
     def create_deuce_phrase(self, player: Player):
         return {
@@ -18,44 +32,11 @@ class TennisGame2:
 
         if self.player1.points == self.player2.points:
             result = self.create_deuce_phrase(self.player1)
-        elif (
-            self.player1.points > 0
-            and self.player2.points == 0
-            and self.player1.points < 4
-        ):
-            result = {1: "Fifteen", 2: "Thirty", 3: "Forty"}.get(
-                self.player1.points, ""
-            ) + "-Love"
-
-        elif (
-            self.player2.points > 0
-            and self.player1.points == 0
-            and self.player2.points < 4
-        ):
-            result = "Love-" + {1: "Fifteen", 2: "Thirty", 3: "Forty"}.get(
-                self.player2.points, ""
-            )
-
-        elif (
-            self.player1.points > self.player2.points
-            and self.player2.points < 3
-            and self.player1.points < 4
-        ):
+        elif self._is_regular_score():
             result = (
-                {1: "Fifteen", 2: "Thirty", 3: "Forty"}.get(self.player1.points, "")
+                self.POINTS_TO_SCORE[self.player1.points]
                 + "-"
-                + {0: "Love", 1: "Fifteen", 2: "Thirty"}.get(self.player2.points, "")
-            )
-
-        elif (
-            self.player2.points > self.player1.points
-            and self.player1.points < 3
-            and self.player2.points < 4
-        ):
-            result = (
-                {0: "Love", 1: "Fifteen", 2: "Thirty"}.get(self.player1.points, "")
-                + "-"
-                + {1: "Fifteen", 2: "Thirty", 3: "Forty"}.get(self.player2.points, "")
+                + self.POINTS_TO_SCORE[self.player2.points]
             )
 
         # Win conditions
