80604350d3079cb44595635ab406f27a33a4dafd
NicaZadirieva <no.demchenko@yandex.ru>
2026-08-20 15:55:48 +0300

    refactor: выделены enum

--- python/tennis4.py
+++ python/tennis4.py
@@ -1,4 +1,14 @@
+from enum import Enum, auto
 from player import Player
+
+
+class GameStatus(Enum):
+    REGULAR = auto()
+    DEUCE = auto()
+    SERVER_ADVANTAGE = auto()
+    RECEIVER_ADVANTAGE = auto()
+    SERVER_WIN = auto()
+    RECEIVER_WIN = auto()
 
 
 class TennisGame4:
@@ -9,53 +19,55 @@ class TennisGame4:
         self.receiver = receiver
 
     def score(self):
-        if GameState.is_deuce(self.server, self.receiver):
+        status = GameState.status(self.server, self.receiver)
+        if status == GameStatus.DEUCE:
             return TennisResultFormatter.format("Deuce", "")
-        if GameState.server_has_won(self.server, self.receiver):
+        if status == GameStatus.SERVER_WIN:
             return TennisResultFormatter.format("Win for " + self.server.name, "")
-        if GameState.receiver_has_won(self.server, self.receiver):
+        if status == GameStatus.RECEIVER_WIN:
             return TennisResultFormatter.format("Win for " + self.receiver.name, "")
-        if GameState.server_has_advantage(self.server, self.receiver):
+        if status == GameStatus.SERVER_ADVANTAGE:
             return TennisResultFormatter.format("Advantage " + self.server.name, "")
-        if GameState.receiver_has_advantage(self.server, self.receiver):
+        if status == GameStatus.RECEIVER_ADVANTAGE:
             return TennisResultFormatter.format("Advantage " + self.receiver.name, "")
-
-        return TennisResultFormatter.format(
-            self.SCORES[self.server.points], self.SCORES[self.receiver.points]
-        )
+        if status == GameStatus.REGULAR:
+            return TennisResultFormatter.format(
+                self.SCORES[self.server.points], self.SCORES[self.receiver.points]
+            )
 
 
 class GameState:
     @staticmethod
-    def is_deuce(server: Player, receiver: Player):
-        return (
+    def status(server: Player, receiver: Player) -> GameStatus:
+        # Сначала проверяем, не закончился ли гейм
+        if server.points >= 4 and (server.points - receiver.points) >= 2:
+            return GameStatus.SERVER_WIN
+        if receiver.points >= 4 and (receiver.points - server.points) >= 2:
+            return GameStatus.RECEIVER_WIN
+
+        # Теперь проверяем преимущество (нужно минимум 4 очка и разница 1)
+        if server.points >= 4 and (server.points - receiver.points) == 1:
+            return GameStatus.SERVER_ADVANTAGE
+        if receiver.points >= 4 and (receiver.points - server.points) == 1:
+            return GameStatus.RECEIVER_ADVANTAGE
+
+        # Деус: оба >= 3 и равны
+        if (
             server.points >= 3
             and receiver.points >= 3
-            and (server.points == receiver.points)
-        )
+            and server.points == receiver.points
+        ):
+            return GameStatus.DEUCE
 
-    @staticmethod
-    def server_has_won(server: Player, receiver: Player):
-        return server.points >= 4 and (server.points - receiver.points) >= 2
-
-    @staticmethod
-    def receiver_has_won(server: Player, receiver: Player):
-        return receiver.points >= 4 and (receiver.points - server.points) >= 2
-
-    @staticmethod
-    def server_has_advantage(server: Player, receiver: Player):
-        return server.points >= 4 and (server.points - receiver.points) == 1
-
-    @staticmethod
-    def receiver_has_advantage(server: Player, receiver: Player):
-        return receiver.points >= 4 and (receiver.points - server.points) == 1
+        # Обычный счёт
+        return GameStatus.REGULAR
 
 
 class TennisResultFormatter:
     @staticmethod
-    def format(server_result: str, receiver_result: str):
-        if receiver_result == "":
+    def format(server_result: str, receiver_result: str) -> str:
+        if not receiver_result:
             return server_result
         if server_result == receiver_result:
-            return server_result + "-All"
-        return server_result + "-" + receiver_result
+            return f"{server_result}-All"
+        return f"{server_result}-{receiver_result}"
